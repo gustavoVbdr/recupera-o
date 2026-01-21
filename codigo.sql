92c8f9d6b0339116b8d0c964bd837c90fbb12294
gustavoVbdr <gustavo.rodrigues@sptech.school>
2026-01-21 13:12:48 -0300

    recGustavo

---
+++ codigo.sql
@@ -0,0 +1,38 @@
+
+CREATE DATABASE livrariaRecuperacao20261;
+
+
+USE livrariaRecuperacao20261;
+
+CREATE TABLE autor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50)
+);
+
+CREATE TABLE genero (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50)
+);
+
+CREATE TABLE livro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(50),
+    fkAutor INT,
+    fkGenero INT,
+    precoCompra DOUBLE,
+    precoVenda DOUBLE,
+    estoqueQnt int not null,
+    CONSTRAINT fk_livro_autor FOREIGN KEY (fkAutor) REFERENCES autor(id),
+    CONSTRAINT fk_livro_genero FOREIGN KEY (fkGenero) REFERENCES genero(id)
+);
+
+insert into genero(nome) values
+("horror"),
+("romance"),
+("poesia"),
+("fantasia");
+
+select * from livro;
+
+
+
---
+++ src/models/livrosModel.js
@@ -0,0 +1,47 @@
+var database = require("../database/config");
+
+function listar() {
+
+    var instrucaoSql = `
+        select 
+livro.id,
+livro.titulo,
+livro.precoCompra,
+livro.precoVenda,
+autor.nome as nomeAutor,
+genero.nome as nomeGenero
+ from livro join autor on autor.id = livro.fkAutor join genero on genero.id = livro.fkGenero;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+
+function cadastrar(titulo, fkAutor, fkGenero, precoCompra, precoVenda, estoqueQnt) {
+
+    var instrucaoSql = `
+        INSERT INTO livro (titulo, fkAutor, fkGenero, precoCompra, precoVenda, estoqueQnt) VALUES ('${titulo}', '${fkAutor}', '${fkGenero}', '${precoCompra}', '${precoVenda}','${estoqueQnt}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function editar(novoPrecoCompra, novoPrecoVenda, id) {
+
+    var instrucaoSql = `
+        UPDATE livro 
+        SET precoCompra = '${novoPrecoCompra}', 
+            precoVenda = '${novoPrecoVenda}'
+        WHERE id = ${id};
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    listar,
+    cadastrar,
+    editar
+}
